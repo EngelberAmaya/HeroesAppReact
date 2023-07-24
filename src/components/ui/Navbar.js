d99e8599dc77911b61fe528c7ce08d22f9c9449b
EngelberAmaya <engelberamaya@gmail.com>
2023-07-24 19:04:42 -0400

    Cards with hero information

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -3,8 +3,8 @@ import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">            
+
             <Link 
                 className="navbar-brand" 
                 to="/"
@@ -12,7 +12,13 @@ export const Navbar = () => {
                 Asociaciones
             </Link>
 
-            <div className="navbar-collapse">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" 
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <div className="navbar-nav">
 
                     <NavLink 
