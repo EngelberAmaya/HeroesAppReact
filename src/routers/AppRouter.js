d914077618de32ff7d0eb518a0f9d7a999b8c5ba
EngelberAmaya <engelberamaya@gmail.com>
2023-07-27 11:13:56 -0400

    Private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,21 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import DashboardRoutes from './DashboardRoutes';
 import LoginScreen from '../components/login/LoginScreen';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
+
     return (
         <BrowserRouter>            
             <Routes>                
                 <Route path="/login" element={<LoginScreen />} />
-                <Route path="/*" element={ <DashboardRoutes /> } />
+                
+                <Route path="/*" element={ 
+                        <PrivateRoute>
+                            <DashboardRoutes />
+                        </PrivateRoute>
+                    } 
+                />
             </Routes>
         </BrowserRouter>
     )
