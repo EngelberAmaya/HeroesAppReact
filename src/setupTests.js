5a629a457873a198334b27398cd274bbf65cc807
EngelberAmaya <engelberamaya@gmail.com>
2023-07-29 16:14:44 -0400

    Testing in the authReducer

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import {createSerializer} from 'enzyme-to-json';
+
+//enzyme
+import Enzyme from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+Enzyme.configure({ adapter: new Adapter() });
+
+//enzyme-to-json
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
